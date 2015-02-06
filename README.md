49a7f214ce721fd431c5f2d45c4316268f2c126a
emontes <emontes@turista.com.mx>
2015-02-06 14:39:07 -0500

    Incudes arrays for photo management

--- README.md
+++ README.md
@@ -5,6 +5,5 @@ Introduction
 ------------
 This is a Photo Gallery taking data from Flickr.  It doesn't use any database and its made in Zend Framework 2.
 
-Installation
-------------
+You can see a working demo in www.pueblapictures.com
 
--- module/Home/src/Home/Controller/PhotoController.php
+++ module/Home/src/Home/Controller/PhotoController.php
@@ -25,51 +25,17 @@ class PhotoController extends AbstractActionController
         
         
         $idarray = explode('+', $id);
-        if (count($idarray > 0)) {
-          $id = $idarray[0];  
-          $photoDetails2 = $photoService->getPhotoDetails($flickr, $idarray[1]);
-//                   $debug = new Debug();
-//                   $debug->dump($photoDetails2);
-//                   die();
-          $photoExif2 = $photoService->getPhotoExif($flickr, $idarray[1]);
-          $photoInfo2 = $photoService->getPhotoInfo($flickr, $idarray[1]);
-          $photoDetails3 = $photoService->getPhotoDetails($flickr, $idarray[2]); //$flickr->getImageDetails($id);
-          $photoExif3 = $photoService->getPhotoExif($flickr, $idarray[2]);
-          $photoInfo3 = $photoService->getPhotoInfo($flickr, $idarray[2]);
-        }  
-        
-        $photoDetails = $photoService->getPhotoDetails($flickr, $id); //$flickr->getImageDetails($id);
-        $photoExif = $photoService->getPhotoExif($flickr, $id);
-        $photoInfo = $photoService->getPhotoInfo($flickr, $id);
-        
-        if (count($idarray > 0)) {
-        $retArray = array(
-            'photoInfo'    => $photoInfo,
-            'photoDetails' => $photoDetails,
-            'photoExif'    => $photoExif,
-            'photoInfo2'    => $photoInfo2,
-            'photoDetails2' => $photoDetails2,
-            'photoExif2'    => $photoExif2,
-            'photoInfo3'    => $photoInfo3,
-            'photoDetails3' => $photoDetails3,
-            'photoExif3'    => $photoExif3,
-            'photo_id3'     => $id);
-        } else {
-            $retArray = array(
-                'photoInfo'    => $photoInfo,
-                'photoDetails' => $photoDetails,
-                'photoExif'    => $photoExif,
-                'photo_id'     => $id);
+        $photos = array();
+        foreach ($idarray as $id) {
+            $photos[] = array(
+                'id'      => $id,  
+                'details' => $photoService->getPhotoDetails($flickr, $id),
+                'info'    => $photoService->getPhotoInfo($flickr, $id),
+                'exif'    => $photoService->getPhotoExif($flickr, $id),
+            );
         }
-       
         
-        
-        
-       
-        
-        
-        
-        return $retArray;
+        return array('photos' => $photos);
        
     }
 }
